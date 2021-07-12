46961e0d3518bf3bbf4b7dfbaa72f9551e410d5a
Simon van Bernem <simon.vanbernem@yahoo.de>
2021-07-12 23:37:32 +0200

    Made the example multithreaded

--- main.cpp
+++ main.cpp
@@ -7,120 +7,155 @@
 #include "stdio.h"
 #include "math.h"
 
+const int number_of_jobs = 20;
 const int number_of_workers = 4;
 
-void* base_fiber;
-
-double random_state = 0.4;
-
-double get_double_between_0_and_1(){
-	//the shittiest rng I have ever written, but its predictable so you can reproduce things.
-	random_state = (random_state * 31.4 + 0.12) / 1.7;
-	random_state = fmod(random_state, 1);
-	return random_state;
-}
-
-struct Worker_Data{
+struct Job_Data{
 	void* fiber;
 	int id;
 	double time_when_network_is_done;
-	bool has_worker_started = false;
-	bool is_worker_done = false;
+	bool has_job_started = false;
+	bool is_job_done = false;
+	bool can_be_taken_by_a_worker = true;
+	HANDLE access_mutex;
 };
 
-void worker_yield(Worker_Data* worker_data){
-	printf("worker %d yields\n", worker_data->id);
-	SwitchToFiber(base_fiber);
-	printf("worker %d resumed\n", worker_data->id);
+struct Worker_Data{
+	void* base_fiber;
+	double random_state;
+	int id;
+	HANDLE thread_handle;
+};
+
+thread_local Worker_Data* worker_data;
+
+Job_Data global_job_pool[number_of_jobs] = {};
+
+double get_double_between_0_and_1(){
+	//the shittiest rng I have ever written, but its predictable so you can reproduce things.
+	worker_data->random_state = (worker_data->random_state * 31.4 + 0.12) / 1.7;
+	worker_data->random_state = fmod(worker_data->random_state, 1);
+	return worker_data->random_state;
 }
 
-void schedule_worker(Worker_Data* worker_data){
-	SwitchToFiber(worker_data->fiber); //apparently stepping in the debugger won't let you enter the fiber execution. You have to actually set a breakpoint
+
+void job_yield(Job_Data* job_data){
+	// printf("job %d yields\n", job_data->id);
+	SwitchToFiber(worker_data->base_fiber);
+	// printf("job %d resumed\n", job_data->id);
 }
 
-bool is_network_call_done(Worker_Data* worker_data){
-	return (double)clock()/(double)CLOCKS_PER_SEC > worker_data->time_when_network_is_done;
+void schedule_job(Job_Data* job_data){
+	SwitchToFiber(job_data->fiber); //apparently stepping in the debugger won't let you enter the fiber execution. You have to actually set a breakpoint
 }
 
-void start_network_call_and_yield(Worker_Data* worker_data){
+bool is_network_call_done(Job_Data* job_data){
+	return (double)clock()/(double)CLOCKS_PER_SEC > job_data->time_when_network_is_done;
+}
+
+void start_network_call_and_yield(Job_Data* job_data){
 	//a common use case for co-routines and fibers and the likes is that you do asynchronous network operations, but structure your code like they were synchronous. Instead of an expensive network call, we just say that the network call will take between 1 and 5 seconds.
 
 	double time_until_network_call_is_done = get_double_between_0_and_1() * 4. + 1.;
-	worker_data->time_when_network_is_done = (double)clock()/(double)CLOCKS_PER_SEC + time_until_network_call_is_done;
+	job_data->time_when_network_is_done = (double)clock()/(double)CLOCKS_PER_SEC + time_until_network_call_is_done;
 
-	worker_yield(worker_data);
+	job_yield(job_data);
 }
 
-void compute_some_important_value(int* value){
-	for(int i = 0; i < 1000 * 1000 * 10; i++)
+void compute_some_important_value(unsigned* value){
+	for(int i = 0; i < 1000 * 1000 * 30; i++)
 		*value += i * 2 - i / 3;
 }
 
-void worker_main(void* parameter){
-	auto worker_data = (Worker_Data*) parameter;
+void job_main(void* parameter){
+	auto job_data = (Job_Data*) parameter;
 
-	worker_data->has_worker_started = true;
+	job_data->has_job_started = true;
 
-	printf("worker %d started\n", worker_data->id);
+	printf("worker %d: job %d started\n", worker_data->id, job_data->id);
 
-	int value = worker_data->id;
-
-	compute_some_important_value(&value);
-	start_network_call_and_yield(worker_data);
-	printf("worker %d finished network call 1\n", worker_data->id);
+	unsigned value = job_data->id;
 
 	compute_some_important_value(&value);
-	start_network_call_and_yield(worker_data);
-	printf("worker %d finished network call 2\n", worker_data->id);
+	start_network_call_and_yield(job_data);
+	printf("worker %d: job %d finished network call 1\n", worker_data->id, job_data->id);
 
 	compute_some_important_value(&value);
-	start_network_call_and_yield(worker_data);
-	printf("worker %d finished network call 3\n", worker_data->id);
+	start_network_call_and_yield(job_data);
+	printf("worker %d: job %d finished network call 2\n", worker_data->id, job_data->id);
+
+	compute_some_important_value(&value);
+	start_network_call_and_yield(job_data);
+	printf("worker %d: job %d finished network call 3\n", worker_data->id, job_data->id);
 
 	compute_some_important_value(&value);
 
-	printf("worker %d finished with value %d\n", worker_data->id, value % 100);
-	worker_data->is_worker_done = true;
+	printf("worker %d: job %d finished with value %u\n", worker_data->id, job_data->id, value % 100);
+	job_data->is_job_done = true;
 
-	worker_yield(worker_data);
+	job_yield(job_data);
 }
 
-int main(){
-	base_fiber = ConvertThreadToFiber(nullptr);
-	assert(base_fiber != nullptr /*ConvertThreadToFiber failed. We need to convert the base thread to a fiber first, because only a fiber can schedule other fibers.*/);
+DWORD WINAPI worker_main(void* parameter){
+	worker_data = (Worker_Data*) parameter;
 
-	Worker_Data workers[number_of_workers] = {};
-
-	for(int i = 0; i < number_of_workers; i++){
-		workers[i].id = i;
-		workers[i].fiber = CreateFiber(2000000 /*stack size*/, &worker_main /*function to start fiber in*/, &workers[i] /*parameter for fiber start function*/);
-		assert(base_fiber != nullptr /*Creating a fiber for a worker failed.*/);
-	}
+	worker_data->base_fiber = ConvertThreadToFiber(nullptr);
+	assert(worker_data->base_fiber != nullptr /*ConvertThreadToFiber failed. We need to convert the base thread to a fiber first, because only a fiber can schedule other fibers.*/);
 
 	int value;
 
-	printf("starting workers\n");
+	printf("worker %d: starting jobs\n", worker_data->id);
 	while(true){
-		for(int i = 0; i < number_of_workers; i++){
-			if(!workers[i].is_worker_done && (!workers[i].has_worker_started || is_network_call_done(&workers[i])))
-				schedule_worker(&workers[i]);
+		for(int i = 0; i < number_of_jobs; i++){
+			auto job = &global_job_pool[i];
+			DWORD mutex_access_result = WaitForSingleObject(job->access_mutex, 0);
 
-			compute_some_important_value(&value); //just here so that there is a visible gap between fiber schedules in the profiler
+			if(mutex_access_result == WAIT_OBJECT_0){ //we got the mutex
+				if(!job->is_job_done && (!job->has_job_started || is_network_call_done(job)))
+					schedule_job(job);
+
+				ReleaseMutex(job->access_mutex);
+			}
 		}
 
-		bool all_workers_done = true;
+		bool all_jobs_done = true;
 
-		for(int i = 0; i < number_of_workers; i++)
-			all_workers_done = all_workers_done && workers[i].is_worker_done;
+		for(int i = 0; i < number_of_jobs; i++)
+			all_jobs_done = all_jobs_done && global_job_pool[i].is_job_done;
 
-		if(all_workers_done)
+		if(all_jobs_done)
 			break;
 	}
 
-	for(int i = 0; i < number_of_workers; i++)
-		DeleteFiber(workers[i].fiber);
+	printf("worker %d: all jobs finished\n", worker_data->id);
 
-	printf("all workers finished\n");
+	return 0;
+}
+
+void main(){
+	for(int i = 0; i < number_of_jobs; i++){
+		global_job_pool[i].id = i;
+		global_job_pool[i].fiber = CreateFiber(2000000 /*stack size*/, &job_main /*function to start fiber in*/, &global_job_pool[i] /*parameter for fiber start function*/);
+		global_job_pool[i].access_mutex = CreateMutex(NULL, 0, NULL);
+	}
+
+	HANDLE handles_to_wait_on[number_of_workers];
+
+	Worker_Data workers[number_of_workers];
+
+	for(int i = 0; i < number_of_workers; i++){
+		workers[i].id = i;
+		workers[i].thread_handle = CreateThread(NULL, 2000000, &worker_main, &workers[i], 0, NULL);
+		workers[i].random_state = fmod(0.28 * (double) i, 1);
+		handles_to_wait_on[i] = workers[i].thread_handle;
+	}
+
+	WaitForMultipleObjects(number_of_workers, &handles_to_wait_on[0], 1, INFINITE);
+
+	for(int i = 0; i < number_of_jobs; i++)
+		DeleteFiber(global_job_pool[i].fiber);
+
+	printf("all jobs and workers finished\n");
 }
 
 
